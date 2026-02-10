bfd135c13c339828ca70b3289df61924757ae805
Veasna <sna4yu@gmail.com>
2026-02-10 08:31:30 +0700

    add the serch bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,15 +12,15 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: const EdgeInsets.only(top: 20, left: 15),
+        margin: const EdgeInsets.only(top: 28, left: 15),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Text(
-              "Location device",
+              "Location ",
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 5),
+            const SizedBox(height: 6),
             Row(
               children: const [
                 Icon(
@@ -30,23 +30,46 @@ class _HomeState extends State<Home> {
                 ),
                 SizedBox(width: 5),
                 Text(
-                  "San Francisco, CA",
+                  "Phnom Penh, Cambodia",
                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
               ],
             ),
             const SizedBox(height: 20.0),
-            Container(
-              margin: const EdgeInsets.only(right: 15),
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: const Color.fromARGB(255, 245, 100, 100),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.only(left: 15),
+                    margin: const EdgeInsets.only(right: 15),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: const Color.fromARGB(141, 97, 97, 97),
+                        width: 1.5,
+                      ),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search for products",
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 11,
+                          horizontal: 11,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-              child: TextField(
-                decoration: InputDecoration(border: InputBorder.none),
-              ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 34, 41, 47),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Icon(Icons.search, color: Colors.white, size: 24),
+                ),
+              ],
             ),
           ],
         ),
